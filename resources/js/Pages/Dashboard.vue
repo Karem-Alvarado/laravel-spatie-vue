556b9535eba116208e14a2cfcb4eccfaf9135adb
Karem-Alvarado <alvaradoalfarokarem@gmail.com>
2024-09-27 17:27:34 -0400

    cambio en el controlador

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -10,7 +10,7 @@ import Welcome from '@/Components/Welcome.vue';
                 Dashboard
             </h2>
         </template>
-<Welcome />
+    <Welcome/>
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
@@ -18,14 +18,12 @@ import Welcome from '@/Components/Welcome.vue';
                         <div class="grid gap-6 lg:grid-cols-2 lg:gap-8">
                             <div class="relative flex items-center gap-6 lg:items-end">
                                 <div id="docs-card-content" class="flex items-start gap-6 lg:flex-col">
-
-
                                     <div class="flex flex-col items-start gap-6 overflow-hidden rounded-lg bg-white p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] transition duration-300 hover:text-black/70 hover:ring-black/20 focus:outline-none focus-visible:ring-[#FF2D20] md:row-span-3 lg:p-10 lg:pb-10 dark:bg-zinc-900 dark:ring-zinc-800 dark:hover:text-white/70 dark:hover:ring-zinc-700 dark:focus-visible:ring-[#FF2D20]">
                                         <div class="relative flex items-center gap-6 lg:items-end">
                                             <div id="docs-card-content" class="flex items-start gap-6 lg:flex-col">
                                                 <div class="pt-3 sm:pt-5 lg:pt-0">
                                                     <!-- size-6 shrink-0 stroke-[#FF2D20]" -->
-                                                    <h2 class="text-xl font-semibold text-black dark:text-white">Instalación Laravel-vue-inertia</h2>
+                                                    <h2 class="text-xl font-semibold text-black dark:text-white">Instalación Laravel-vue-inertia v11.25</h2>
                                                     <p class="mt-4 text-sm/relaxed">
                                                         composer create-project laravel/laravel nombre-proyecto
                                                     </p>
